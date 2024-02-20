fb2be20caab8f8e999782ef09c18810c15c34ef7
C046 <79424595+C046@users.noreply.github.com>
2024-02-20 10:58:25 -0800

    Update NetworkUsage.py

--- NetworkUsage.py
+++ NetworkUsage.py
@@ -6,6 +6,7 @@ Created on Mon Feb 19 17:35:28 2024
 """
 import numpy as np
 import pandas as pd
+from mpmath import mp
 
 from OrganizedNeuralNetwork.Input import InputLayer
 from OrganizedNeuralNetwork.Hidden import HiddenLayer
@@ -16,53 +17,54 @@ from OrganizedNeuralNetwork.Norm import Normalization
 # Data
 data = pd.read_csv("OrganizedNeuralNetwork/breast-cancer.csv")
 tumor_descriptions = data.drop('diagnosis', axis=1)
+tumor_descriptions = tumor_descriptions.applymap(mp.mpf)
+#mpf_array = mp.matrix(tumor_descriptions.values)
+
+# Convert 'M' and 'B' to 1 and 0 in y_true
 y_true = (data['diagnosis'].values == 'M').astype(int)
 
 
 # Input array
-neurons = np.array([i for i in range(0,100)])
 
-# Input layer pipeline
-input_layer = InputLayer(neurons, batch_size=5)
+neurons = tumor_descriptions["id"]
 
-# Hidden layer pipeline
-hidden_layer = HiddenLayer()
-
-# Output layer pipeline
-output_layer = OutputLayer()
 
 # Normalization functions
 normal = Normalization(neurons)
 
-
-num_hidden_layers = 10
-for i in range(num_hidden_layers):    
-    output = []
-    # Example of using a single loop for both batched inputs and activations
-    for batched_inputs in input_layer.batch_inputs():
-        print("\nBatch Elements:", batched_inputs)
+# Input layer pipeline
+for column_name, neurons in tumor_descriptions.items():
+    input_layer = InputLayer(neurons, batch_size=569)
     
-        # Activator functions
-        activators = Activations(batched_inputs)
+    num_hidden_layers = 10
+    for i in range(num_hidden_layers):    
+        output = []
+        # Example of using a single loop for both batched inputs and activations
+        for batched_inputs in input_layer.batch_inputs():
+            #print("\nBatch Elements:", batched_inputs)
     
-        for neuron in activators.Iter_neuron():
+            # Activator functions
+            activators = Activations(batched_inputs)
+    
+            for neuron in activators.Iter_neuron():
             
-            neuron,bias,weights = neuron
-            Neuron_weighted_sum = np.dot(weights, neuron)+ bias
-            print("Neuron:", neuron,"\nBias:", bias,"Weights:", weights, f"\nNeuron Weighted Sum: {Neuron_weighted_sum}")
+                neuron,bias,weights = neuron
+                Neuron_weighted_sum = np.dot(weights, neuron)+ bias
+                #print("Neuron:", neuron,"\nBias:", bias,"Weights:", weights, f"\nNeuron Weighted Sum: {Neuron_weighted_sum}")
         
         
-            output.append(activators.Sigmoid(Neuron_weighted_sum))
+                output.append(activators.Sigmoid(Neuron_weighted_sum))
         
-        neurons = output
-        norm = Normalization(neurons).binary_cross_entropy(y_true)
-        print(f"\n\nSigmoid: {output} \n{norm}") 
+        
+            neurons = np.array(output)
+        
+            # # Iterate through columns
+            # for column_name, neurons in tumor_descriptions.items():
+            norm = Normalization(neurons).binary_cross_entropy(y_true)
+            
+            print(f"\nNormal: {norm}") 
+            
     
-# # Hidden layer pipeline
-Hidden_Layer = HiddenLayer()
-# # Outpudt layer pipeline
-Output_Layer = OutputLayer()
-
 
 # # Normal functions
 # Normals = Normalization()
